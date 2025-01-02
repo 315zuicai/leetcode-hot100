183761e027a2cb982ffc9b746444c69c42019199
dingkaiqiang <dingkaiqiang@baidu.com>
2025-01-02 18:47:54 +0800

    feat: 56、189

---
+++ src/189-rotate.ts
@@ -0,0 +1,65 @@
+/**
+ * 189. 轮转数组
+ * 
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * 
+ * 示例 1:
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释: 向右轮转 3 步: [7,1,2,3,4,5,6] -> [6,7,1,2,3,4,5] -> [5,6,7,1,2,3,4]
+ * 
+ * 示例 2:
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释: 向右轮转 2 步: [99,-1,-100,3] -> [3,99,-1,-100]
+ * 
+ * 提示：
+ * - 1 <= nums.length <= 10^5
+ * - -2^31 <= nums[i] <= 2^31 - 1
+ * - 0 <= k <= 10^5
+ * 
+ * 进阶：
+ * - 尝试使用空间复杂度为 O(1) 的原地算法解决这个问题
+ * 
+ * Do not return anything, modify nums in-place instead.
+ */
+// function rotate(nums: number[], k: number): void {
+//     const len = nums.length;
+//     for (let i = 0; i < k; i++) {
+//         const right = nums.pop()!;
+//         nums.unshift(right);
+//     }
+// };
+// function rotate(nums: number[], k: number): void {
+//     k = k % nums.length;
+//     const reverse = (nums: number[], start: number, end: number) => {
+//         while (start < end) {
+//             [nums[start], nums[end]] = [nums[end], nums[start]];
+//             start++;
+//             end--;
+//         }
+//     }
+//     reverse(nums, 0, nums.length - 1);
+//     reverse(nums, 0, k - 1);
+//     reverse(nums, k, nums.length - 1);
+// };
+// 关键逻辑 目标位置=(i+k)%n
+function rotate(nums: number[], k: number): void {
+    const len = nums.length;
+    k = k % len;
+    if (k === 0 || len <= 1) return;
+    let count = 0;
+    for (let start = 0; count < len; start++) {
+        let cur = start;
+        let prev = nums[start];
+        do {
+            const next = (cur + k) % len;
+            [prev, nums[next]] = [nums[next], prev];
+            cur = next;
+            count++;
+        } while (start !== cur)
+    }
+    console.log(nums);
+};
+const nums = [1, 2, 3, 4, 5, 6, 7], k = 3;
+rotate(nums, k)
---
+++ src/56-merge.ts
@@ -0,0 +1,40 @@
+/**
+ * 56. 合并区间
+ * 
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi]。
+ * 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ * 
+ * 示例 1：
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6]。
+ * 
+ * 示例 2：
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * 
+ * 提示：
+ * - 1 <= intervals.length <= 10^4
+ * - intervals[i].length == 2
+ * - 0 <= starti <= endi <= 10^4
+ */
+function merge(intervals: number[][]): number[][] {
+    if (intervals.length <= 1) {
+        return intervals;
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
+    const res: number[][] = [intervals[0]];
+    for (let i = 1; i < intervals.length; i++) {
+        const maxEdge = res[res.length - 1][1];
+        if (maxEdge >= intervals[i][0]) {
+            res[res.length - 1][1] = Math.max(maxEdge, intervals[i][1]);
+        } else {
+            res.push(intervals[i])
+        }
+    }
+    return res;
+};
+
+const intervals = [[1, 4], [0, 2], [3, 5]]
+console.log(merge(intervals));
